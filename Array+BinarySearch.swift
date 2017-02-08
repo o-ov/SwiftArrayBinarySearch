d946d09cbc7444db96792f690a03288680679081
o-ov <zhangfan1@live.com>
2017-02-08 17:06:55 +0800

    Add Swift file
    Add Swift file

---
+++ Array+BinarySearch.swift
@@ -0,0 +1,54 @@
+//
+//  Array+BinarySearch.swift
+//
+//  Created by Stan Zhang on 2017/2/8.
+//  Copyright © 2017年 Stan Zhang.
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+
+import Foundation
+
+extension Array where Element: Comparable {//the element of the Array should be Comparable
+    
+    func binarySearch(element: Element) -> Int? {        
+        var low = 0
+        var high = count
+        var mid = Int(high / 2)
+        
+        while low < high {
+            
+            let midElement = self[mid]
+            
+            if element == midElement {
+                return mid
+            }
+            else if element < midElement {
+                high = mid
+            }
+            else {
+                low = mid
+            }
+            
+            mid = (low + high) / 2
+        }
+        
+        return nil
+    }
+}
